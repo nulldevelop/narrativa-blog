618e09b23476fc03f03d0812999af11b49944f49
devpedroA <pedroandradegt25@gmail.com>
2026-04-09 22:31:02 -0300

    .

--- src/app/(auth)/dashboard-owner/modules/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ModulesList } from './_components/modules-list'
-
-export default function ModulesPage() {
-	return (
-		<div className="flex min-h-svh flex-col gap-2  p-4">
-			<ModulesList />
-		</div>
-	)
-}
